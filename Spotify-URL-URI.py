282818bee608f41747bcdbc8e584eca3e81cf8b1
Jose Perez <jose@joseperez.fm>
2023-05-02 13:42:30 -0700

    Update Spotify-URL-URI.py
    Rewritten with far less code duplication. Drops Python 2 support since no longer needed in new versions of Python.

--- Spotify-URL-URI.py
+++ Spotify-URL-URI.py
@@ -3,65 +3,44 @@ import os
 
 inp = "{query}"
 
-#  URLs to URIs
-#  Converts track URL to URI.
-if re.search("/track/", inp):
-    processed = inp.split('track/')
-    uri = "spotify:track:" + processed[-1][0:22]
-    cmd = 'echo %s | tr -d "\n" | pbcopy' % uri
-    os.system(cmd)
+def convert_url_to_uri(inp, key):
+    processed = inp.split(f'/{key}/')
+    return f"spotify:{key}:" + processed[-1].split('?')[0]
 
-#  Converts artist URL to URI.
-elif re.search("/artist/", inp):
-    processed = inp.split('artist/')
-    uri = "spotify:artist:"+processed[-1][0:22]
-    cmd = 'echo %s | tr -d "\n" | pbcopy' % uri
-    os.system(cmd)
+def convert_uri_to_url(inp, key):
+    processed = inp.split(key)
+    return f"https://open.spotify.com/{key}/" + processed[-1]
 
-#  Converts album URL to URI.
-elif re.search("/album/", inp):
-    processed = inp.split('album')
-    uri = "spotify:album:"+processed[-1][0:22]
-    cmd = 'echo %s | tr -d "\n" | pbcopy' % uri
-    os.system(cmd)
+patterns = {
+    'track': (re.compile("/track/"), re.compile(":track:")),
+    'artist': (re.compile("/artist/"), re.compile(":artist:")),
+    'album': (re.compile("/album/"), re.compile(":album:")),
+    'playlist': (re.compile("/playlist/"), re.compile(":playlist:"))
+}
 
-#  Converts playlist URL to URI
-elif re.search("/playlist/", inp):
-    user = re.search('/user/(.*)/playlist/', inp).group(1)
-    playlist = inp.split('playlist/')
-    uri = "spotify:user:" + user + ":playlist:" + playlist[-1][0:22]
-    cmd = 'echo %s | tr -d "\n" | pbcopy' % uri
-    os.system(cmd)
+for key, (url_pattern, uri_pattern) in patterns.items():
 
-#  URIs to URLs
-#  Converts track URI to URL.
-elif re.search(":track:", inp):
-    processed = inp.split('track:')
-    url = "https://open.spotify.com/track/" + processed[-1][0:22]
-    cmd = 'echo %s | tr -d "\n" | pbcopy' % url
-    os.system(cmd)
-
-#  Converts artist URI to URL.
-elif re.search(":artist:", inp):
-    processed = inp.split('artist:')
-    url = "https://open.spotify.com/artist/" + processed[-1][0:22]
-    cmd = 'echo %s | tr -d "\n" | pbcopy' % url
-    os.system(cmd)
-
-#  Converts album URI to URL.
-elif re.search(":album:", inp):
-    processed = inp.split('album:')
-    url = "https://open.spotify.com/album/" + processed[-1][0:22]
-    cmd = 'echo %s | tr -d "\n" | pbcopy' % url
-    os.system(cmd)
-
-#  Converts playlist URI to URL.
-elif re.search(":playlist:", inp):
-    user = re.search(':user:(.*):playlist:', inp).group(1)
-    playlist = inp.split('playlist:')
-    url = "https://open.spotify.com/user/" + user + "/playlist/" + playlist[-1][0:22]
-    cmd = 'echo %s | tr -d "\n" | pbcopy' % url
-    os.system(cmd)
-
+    if url_pattern.search(inp):
+        if key == "playlist":
+            user_match = re.search('/user/(.*)/playlist/', inp)
+            user = user_match.group(1) if user_match else None
+            uri = "spotify:user:" + user + ":playlist:" + inp.split(f'/playlist/')[-1].split('?')[0] if user else "spotify:playlist:" + inp.split(f'/playlist/')[-1].split('?')[0]
+        else:
+            uri = convert_url_to_uri(inp, key)
+        cmd = f'echo {uri} | tr -d "\n" | pbcopy'
+        os.system(cmd)
+        break
+        
+    elif uri_pattern.search(inp):
+        if key == "playlist":
+            user_match = re.search(':user:(.*):playlist:', inp)
+            user = user_match.group(1) if user_match else None
+            url = "https://open.spotify.com/user/" + user + "/playlist/" + inp.split('playlist:')[-1] if user else "https://open.spotify.com/playlist/" + inp.split('playlist:')[-1]
+        else:
+            url = convert_uri_to_url(inp, key)
+        cmd = f'echo {url} | tr -d "\n" | pbcopy'
+        os.system(cmd)
+        break
+        
 else:
     exit()
